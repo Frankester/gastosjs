7cf1023892e839cafa7e9c8cbadfb02d5d33e27c
frankester <91QAMAvqs>
2022-02-20 02:00:49 -0300

    Adding localstorage persisting

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import Modal from './Components/Modal'
 
 function App() {
   const [title, setTitle] = useState('')
   const [mount, setMount] = useState(0)
   const [cashFlow, setCashFlow] = useState([])
-  const [money, setMoney] = useState('')
+  const [money, setMoney] = useState('0')
+  const [showDetail, setShowDetail] = useState(false)
+  const detailRef = useRef()
 
   const handleChangeTitle = e => {
     setTitle(e.target.value)
   }
   const handleChangeMount = e => {
-    setMount(e.target.value)
+    
+      setMount(Number(e.target.value))
+    
   }
 
   const addEntry = () => {
@@ -19,9 +24,6 @@ function App() {
       setCashFlow(prevExpenses => {
         return [...prevExpenses, { title: title, type:'ingreso', mount: mount}]
       })
-      
-      setMoney(prevMoney => String(Number(prevMoney) + mount))
-
       setTitle('')
       setMount(0)
     }
@@ -32,13 +34,37 @@ function App() {
       setCashFlow(prevExpenses => {
         return [...prevExpenses, { title: title, type:'gasto', mount: mount}]
       })
-
-      setMoney(prevMoney => String(Number(prevMoney) - mount))
-
       setTitle('')
       setMount(0)
     }
   }
+
+  const handleDetail = (e, expense) => {
+    detailRef.current = expense
+    setShowDetail(true)
+  }
+
+  useEffect(() => {
+    const cashFlowLocal = localStorage.getItem('cashFlow')
+    setCashFlow(JSON.parse(cashFlowLocal) || [])
+  },[setCashFlow])
+
+  useEffect(() => {
+    setMoney('0')
+    cashFlow.map(expense => {
+      expense.type ==='ingreso'
+        ?  setMoney(prevMoney => String(Number(prevMoney) + Number(expense.mount)))
+        : expense.type === 'gasto' 
+          ? setMoney(prevMoney => String(Number(prevMoney) - Number(expense.mount)))
+          :null
+    })
+  }, [setMoney,cashFlow])
+
+  useEffect(() => (
+    
+     localStorage.setItem('cashFlow', JSON.stringify(cashFlow))
+    
+  ), [cashFlow])
 
   return (
     <div className="App">
@@ -49,10 +75,20 @@ function App() {
 
       <div className='saldo'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
+           d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" 
+          />
         </svg>
-        <p>{money ? String(Number(money)): '0'}$</p>
+        <p className={Number(money) < 0 ? 'red': ''}>{money ? Number(money) : '0'}$</p>
       </div>
+      {
+        showDetail 
+        ? <Modal expense={detailRef.current}  
+            setCashFlow={setCashFlow}
+            setShowDetail={setShowDetail}
+          />
+        : null 
+      } 
 
       {
         cashFlow.length !== 0
@@ -63,7 +99,7 @@ function App() {
         {
           cashFlow.map((expense, index) => {
             return(
-              <p className='historial-item' key={index}>
+              <p className='historial-item' key={index} onClick={ e => handleDetail(e, expense)}>
                 {expense.title}
                 <span className={expense.type}>{expense.mount}$</span>
               </p>
